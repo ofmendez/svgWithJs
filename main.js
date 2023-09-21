e1651a8146f01f48612e9828cb75219bb80c68be
Fabian Mendez <ofmendez@gmail.com>
2023-09-21 15:59:37 -0500

    corrige redireccion de intenta nuevamente

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.svgElementClicked = (res)=> { window.location.href = res ? b64DecodeUnicode('Li4vcGFnZXMvd2lubmVyLmh0bWw=') : '../pages/tryAgain.html'; }; function b64DecodeUnicode(str) { return decodeURIComponent(atob(str).split('').map(function(c) { return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2); }).join('')); }
 window.svgLoseClicked = ()=>{
-	window.location.href = '/';
+	window.location.href = './../../';
 }
